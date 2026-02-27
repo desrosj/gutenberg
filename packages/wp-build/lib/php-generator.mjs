b693abee3526eb47fde4ae0ff3427334a80aa43d
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2026-02-27 13:10:05 -0500

    Support `wpPlugin.name` without env variables

--- packages/wp-build/lib/php-generator.mjs
+++ packages/wp-build/lib/php-generator.mjs
@@ -30,6 +30,8 @@ export async function getPhpReplacements( rootDir, baseUrlExpression ) {
 	const name =
 		process.env.WP_PLUGIN_NAME ??
 		process.env.npm_package_config_WP_PLUGIN_NAME ??
+		// @ts-expect-error specific override to package.json
+		rootPackageJson.wpPlugin?.name ??
 		'gutenberg';
 	const version = rootPackageJson.version;
 
