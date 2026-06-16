041f390bd39950ba206f28349a7b7d1f3947cc4b
George Mamadashvili <georgemamadashvili@gmail.com>
2026-06-16 08:28:15 +0400

    Add deprecation notices

--- packages/reusable-blocks/src/components/reusable-blocks-menu-items/index.js
+++ packages/reusable-blocks/src/components/reusable-blocks-menu-items/index.js
@@ -1,8 +1,16 @@
 /**
+ * WordPress dependencies
+ */
+import deprecated from '@wordpress/deprecated';
+
+/**
  * Deprecated no-op ReusableBlocksMenuItems component.
  *
  * @return {null} Nothing.
  */
 export default function ReusableBlocksMenuItems() {
+	deprecated( 'wp.reusableBlocks.ReusableBlocksMenuItems', {
+		since: '7.1',
+	} );
 	return null;
 }
